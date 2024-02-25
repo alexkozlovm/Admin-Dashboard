a10a4a06a895efc462f4842567ccb46a91957033
Alex Kozlov <kozlov.a.m@icloud.com>
2024-02-25 17:44:36 -0500

    Added theme switcher

---
+++ index.js
@@ -0,0 +1,27 @@
+//query selectors
+const body = document.querySelector("body");
+
+// Theme switcher
+const sliderCircle = document.querySelector(".slider .circle ");
+const slider = document.querySelector("input[type='checkbox']");
+
+const systemSettingDark = window.matchMedia("(prefers-color-scheme: dark)");
+if (systemSettingDark.matches) darkTheme();
+
+slider.addEventListener('change', choseTheme)
+
+function choseTheme() {
+    if (slider.checked) {
+        darkTheme();
+    } else {
+        lightTheme();
+    }
+}
+function lightTheme() { 
+        slider.checked = false;
+        body.classList.remove("dark");
+}
+function darkTheme() {
+        slider.checked = true;
+        body.classList.add("dark");
+}
